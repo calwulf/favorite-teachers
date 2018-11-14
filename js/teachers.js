25b6998d1d841fa7635f4bba598e5f80dc7e4ed1
calwulf@wl.k12.ia.us <calwulf@wl.k12.ia.us>
2018-11-14 14:43:50 -0600

    set up teacher and constructor

---
+++ js/teachers.js
@@ -0,0 +1,33 @@
+var teachers = [
+
+    {
+        image:"gunn.jpg",
+        name: "Mr. Gunn",
+        subjects: ["Physics"],
+        teacher_id:"0001"
+    },
+    {
+        image:"morris.jpg",
+        name: "Mr .Morris",
+        subjects: ["Floriculture", "Ag Resources"],
+        teacher_id: "0002",
+    },
+    {
+        image:"buckley.jpg",
+        name: "Miss Buckley",
+        subjects: ["Math"],
+        teacher_id: "0003",
+    },
+    {
+        image:"hauschildt.jpg",
+        name: "Mr. Hauschildt",
+        subjects:[ "Marketing", "Buisness Law", "Yearbook"],
+        teacher_id: "0004",
+    },
+    {
+        image:"diemer.jpg",
+        name: "Mr. Diemer",
+        subjects: ["History", "Government"],
+        teacher_id: "0005",
+    },
+];
