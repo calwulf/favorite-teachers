58e6f9c463117b335ea92e867dc24159b07750b6
calwulf@wl.k12.ia.us <calwulf@wl.k12.ia.us>
2018-11-16 14:21:13 -0600

    added stars to teachers

--- js/teacher-constructor.js
+++ js/teacher-constructor.js
@@ -7,7 +7,36 @@ var Teacher = function(teacher){
     this.name = teacher.name;
     this.subjects = teacher.subjects;
     this.teacher_id = teacher.teacher_id;
+    this.stars = teacher.stars;
 };
+Handlebars.registerHelper('star-icons', function(stars){
+    stars = parseInt(stars);
+    var result = '';
+    switch(stars) {
+        case 5: 
+            result += '&#9733;';
+        case 4:
+            result += '&#9733;';
+        case 3:
+            result += '&#9733;';
+        case 2:
+            result += '&#9733;';
+        default:
+            result += '&#9733;';
+    }
+    switch(stars) {
+        case 1:
+            result += '&#9734;';
+        case 2:
+            result += '&#9734;';
+        case 3:
+            result += '&#9734;';
+        case 4: 
+            result += '&#9734;';
+    }
+    return result;
+});
+
 
 Teacher.prototype.toHtml = function(){
     var source = $('#teacher-template').text();
